f03c5f6e04e8db67d55472169467a7c99f7c88e6
luke-h1 <luke.howsam@yahoo.com>
2024-02-14 15:11:22 +0000

    fix(infra): ingress traffic on port 80

--- terraform/module/load_balancer.tf
+++ terraform/module/load_balancer.tf
@@ -35,6 +35,12 @@ resource "aws_security_group" "application_load_balancer_security_group" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   egress {
     from_port   = 0
@@ -84,7 +90,6 @@ resource "aws_security_group" "application_service_security_group" {
     to_port         = 80
     protocol        = "tcp"
     security_groups = ["${aws_security_group.application_load_balancer_security_group.id}"]
-
   }
 
   egress {
