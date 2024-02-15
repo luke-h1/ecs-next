38dc6fbd053ab2acd228b6f5657e226fc87d1fb5
luke-h1 <luke.howsam@yahoo.com>
2024-02-15 13:30:04 +0000

    chore(infra): bump desired infra to 1

--- terraform/live/main.tf
+++ terraform/live/main.tf
@@ -7,6 +7,6 @@ module "app_infra" {
   project_name     = "ecs-next-tf"
   node_env         = "production"
   env              = "live"
-  desired_count    = 3
+  desired_count    = 1
   docker_image_tag = var.docker_image_tag
 }
