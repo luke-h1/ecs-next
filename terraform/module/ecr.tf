7f209cb8fde1f405d4aef8776057c365cbfa14a3
luke-h1 <luke.howsam@yahoo.com>
2024-02-16 15:33:25 +0000

    chore: cleanup infra

--- terraform/module/ecr.tf
+++ terraform/module/ecr.tf
@@ -1,5 +1,6 @@
 resource "aws_ecr_repository" "application_ecr_repo" {
-  name = "${var.project_name}-repo-${var.env}"
+  name         = "${var.project_name}-repo-${var.env}"
+  force_delete = true
   tags = {
     Environment = var.env
     Project     = var.project_name
