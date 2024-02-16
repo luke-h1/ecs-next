b4422b33565343c91104f72d46b7e50d9cc10165
luke-h1 <luke.howsam@yahoo.com>
2024-02-16 17:55:31 +0000

    feat: autoscale

--- load-test.sh
+++ load-test.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-autocannon http://stg.lhowsam.site/ -c 10000  -d 1000 -R 1000
+autocannon https://ecs-next-cxdubo3u1-luke-h1.vercel.app/ -c 1000  -d 1000 -R 1000
+
+
+# 996k requests in 1000.25s, 0 B read
+# 3440k errors (0 timeouts)
--- terraform/module/task.tf
+++ terraform/module/task.tf
@@ -58,3 +58,27 @@ resource "aws_ecs_service" "application_ecs" {
     security_groups  = ["${aws_security_group.application_service_security_group.id}"]
   }
 }
+
+resource "aws_appautoscaling_target" "ecs_target" {
+  max_capacity       = 3
+  min_capacity       = 1
+  resource_id        = "service/${aws_ecs_cluster.application_cluster.name}/${aws_ecs_service.application_ecs.name}"
+  scalable_dimension = "ecs:service:DesiredCount"
+  service_namespace  = "ecs"
+}
+
+resource "aws_appautoscaling_policy" "ecs_policy" {
+  name               = "cpu-utilization"
+  policy_type        = "TargetTrackingScaling"
+  resource_id        = aws_appautoscaling_target.ecs_target.resource_id
+  scalable_dimension = aws_appautoscaling_target.ecs_target.scalable_dimension
+  service_namespace  = aws_appautoscaling_target.ecs_target.service_namespace
+
+  target_tracking_scaling_policy_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ECSServiceAverageCPUUtilization"
+    }
+
+    target_value = 75.0
+  }
+}
