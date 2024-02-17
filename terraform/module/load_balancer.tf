bcb6a2280902f862adb0b5c5f6f0b2ef120d6b0d
luke-h1 <luke.howsam@yahoo.com>
2024-02-17 16:53:37 +0000

    feat(infra): rework alb infra

--- terraform/module/load_balancer.tf
+++ terraform/module/load_balancer.tf
@@ -90,7 +90,7 @@ resource "aws_lb_listener" "web_http" {
   }
 }
 
-resource "aws_lb_listener" "web_https" {
+resource "aws_lb_listener" "application_http" {
   load_balancer_arn = aws_alb.application_load_balancer.arn
   port              = 443
   protocol          = "HTTPS"
@@ -102,16 +102,16 @@ resource "aws_lb_listener" "web_https" {
 }
 
 # redirect 
-resource "aws_lb_listener" "application_http" {
-  load_balancer_arn = aws_alb.application_load_balancer.arn
-  port              = "443"
-  protocol          = "HTTPS"
-  certificate_arn   = "arn:aws:acm:eu-west-2:753493924839:certificate/c96c4e59-bd8e-416d-987d-77929e0f6a23"
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.application_target_group.arn
-  }
-}
+# resource "aws_lb_listener" "application_http" {
+#   load_balancer_arn = aws_alb.application_load_balancer.arn
+#   port              = "443"
+#   protocol          = "HTTPS"
+#   certificate_arn   = "arn:aws:acm:eu-west-2:753493924839:certificate/c96c4e59-bd8e-416d-987d-77929e0f6a23"
+#   default_action {
+#     type             = "forward"
+#     target_group_arn = aws_lb_target_group.application_target_group.arn
+#   }
+# }
 
 resource "aws_route53_record" "alb" {
   zone_id = data.aws_route53_zone.domain.zone_id
