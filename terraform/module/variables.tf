267850e80879edc4c1cc4442c1bcd1027ebb40d4
luke-h1 <luke.howsam@yahoo.com>
2024-02-17 12:52:08 +0000

    perf(infra): bump cpu to 1024

--- terraform/module/variables.tf
+++ terraform/module/variables.tf
@@ -1,25 +1,26 @@
 variable "docker_image_tag" {
-    type = string
-    description = "The tag of the Docker image to deploy"
+  type        = string
+  description = "The tag of the Docker image to deploy"
 }
 
 variable "project_name" {
-    type = string
-    description = "The name of the project"
+  type        = string
+  description = "The name of the project"
 }
 
 variable "env" {
-    type = string
-    description = "The environment to deploy to"
+  type        = string
+  description = "The environment to deploy to"
 }
 
 variable "desired_count" {
-    type = number
-    description = "The number of tasks to run"
+  type        = number
+  description = "The number of tasks to run"
+  default     = 1
 }
 
 variable "node_env" {
-    type = string
-    description = "The node environment"
-    default = "production"
+  type        = string
+  description = "The node environment"
+  default     = "production"
 }
