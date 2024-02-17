ca68c7b11d93cbc26cf13e456f9fccf1e39bef01
luke-h1 <luke.howsam@yahoo.com>
2024-02-17 16:24:08 +0000

    feat(infra): rework alb infra

--- terraform/module/outputs.tf
+++ terraform/module/outputs.tf
@@ -1,4 +1,14 @@
 output "module_ecr_repo_name" {
-    value = aws_ecr_repository.application_ecr_repo.name
-    description = "The name of the ECR repository"
+  value       = aws_ecr_repository.application_ecr_repo.name
+  description = "The name of the ECR repository"
+}
+
+data "terraform_remote_state" "outputs" {
+  backend = "s3"
+  config = {
+    bucket  = "ecs-next-tf-state"
+    key     = "live/terraform.tfstate"
+    region  = "eu-west-2"
+    encrypt = true
+  }
 }
